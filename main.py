f42f5f87c25018336b55545b61d2eeb4c7b942f9
Sean Gallogly <galloglyst149@gmail.com>
2019-12-15 14:19:08 -0600

    updates to main. added skeleton of game loop

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 import numpy as np
 
-def use_free_parking(prompt=""):
+def get_yes_or_no(prompt=""):
     response = input(prompt)
     while response[0].lower() not in ['y', 'n']:
         print("answer must start with 'y' or 'n'. Please try again.")
@@ -37,27 +37,97 @@ while True:
     except ValueError:
        print("You must enter a number to continue!") 
 
-    use_free_parking = use_free_parking("\nNow, would you like to include the " 
+    use_free_parking = get_yes_or_no("\nNow, would you like to include the " 
                                         "free parking pool in your game? "
                                         "(y/n): ")
     print("\nOkay! Let's begin!")
 
+    
+    print("Let's roll the dice to determine who goes first!\n")
+    starting_rolls = []
+    for i in np.arange(num_players):
+        player_i_roll = roll_n_dice(2)
+        print("player "+str(i+1)+" rolls a "+str(player_i_roll[0])+" and a "
+                +str(player_i_roll[1])+".")
+        starting_rolls.append(player_i_roll)
+    starting_rolls = np.array(starting_rolls)
+    sums = [sum(starting_rolls[i]) for i in np.arange(num_players)]
+    # assuming there exists a maximum value...(for now)
+    # we're identifying players as the indices in range(n)
+    first_player = sums.index(max(sums))
+    current_player = first_player
+    
+    #enter the game loop
     while True:
-        print("Let's roll the dice to determine who goes first!\n")
-        starting_rolls = []
-        for i in np.arange(num_players):
-            player_i_roll = roll_n_dice(2)
-            print("player "+str(i+1)+" rolls a "+str(player_i_roll[0])+" and a "
-                  +str(player_i_roll[1])+".")
-            starting_rolls.append(player_i_roll)
-        starting_rolls = np.array(starting_rolls)
-        sums = [sum(starting_rolls[i]) for i in np.arange(num_players)]
-        # assuming there exists a maximum value...(for now)
-        # we're identifying players as the indices in range(n)
-        first_player = sums.index(max(sums))
+        print("\nAll right player", current_player+1, "it's your turn!")
+        user_choice = input("what would you like to do? (r)oll, or (c)heck "
+                            "inventory? ")
+        while user_choice.lower() not in ['r', 'c']:
+            print("Reponse must start with 'r' or 'c'. Try again!")
+            user_choice = input("what would you like to do? (r)oll, or (c)heck "
+                            "inventory? ")
+        
+        if user_choice.lower() == 'c':
+            print("This will be the player's inventory when that functionality"
+                  "is implemented.")
+        else:
+            current_roll = roll_n_dice(2)
+            print("player "+str(current_player+1)+" rolls a "+
+                  str(current_roll[0])+" and a "+str(current_roll[1])+".")
+            
+            #some temp var until implement player and board functions here
+            pass_go = True
+            current_player_cash = 0
+            # temp space codes are as follows:
+            # P = PROPERTY
+            # R = RAILROAD
+            # U = UTILITY
+            # GTJ = GO TO JAIL
+            # CC = COMMUNITY CHEST
+            # C = CHANCE
+            # T = TAXES
+            # FP = FREE PARKING
+            # JV = JUST VISTING
+            space_types = np.array(['P', 'R', 'U', 'GTJ',
+                                    'CC', 'C', 'T', 'FP', 'JV'])
+            current_space = 'P'
+            is_owned = False
 
-        print("\nAll right player", first_player+1, "it's your turn!")
-        user_choice = input("what would you like to do?")
+            if pass_go:
+                current_player_cash += 200
+            
+            if current_space == space_types[0]:
+                if is_owned:
+                    print("This property is owned by (insert player name)! You "
+                          "owe them (amount) in rent.")
+                else:
+                    #TODO: display property name, and stats to player
+                    buy_prop = get_yes_or_no("Looks like this property isn't "
+                                             "owned. would you like to buy this "
+                                             "property? ")
+                    
+                    if buy_prop:
+                        print("You just bought (insert property name)!")
+                    else:
+                        pass
 
+            elif current_space == space_types[1]:
+                pass
+            elif current_space == space_types[2]:
+                pass
+            elif current_space == space_types[3]:
+                pass
+            elif current_space == space_types[4]:
+                pass
+            elif current_space == space_types[5]:
+                pass
+            elif current_space == space_types[6]:
+                pass
+            elif current_space == space_types[7]:
+                pass
+            elif current_space == space_types[8]:
+                pass
 
-
+            current_player = (current_player + 1) % num_players
+            # any other kind of game stats you want to keep track of? such as:
+            # number of turns
